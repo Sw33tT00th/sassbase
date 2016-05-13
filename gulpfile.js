f8a49012cd703e6b421fabc231ae34d0d8126ffd
John Loudon <Sw33tT00th@Johns-MacBook.local>
2016-05-13 15:26:34 -0700

    got it working

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+	gulp.src('./**/*.scss')
+		.pipe(sourcemaps.init())
+			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+			.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./output'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./**/*.scss', ['sass']);
+});
